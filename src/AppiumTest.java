fd4af400217e66a4a51ca182655241c860e277c7
Murodjoni Sharifzoda <murodjoni@Murodjonis-MacBook-Pro.local>
2024-04-27 22:43:55 -0600

    Added Appium test file

---
+++ src/AppiumTest.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class AppiumTest {
+    public static void main(String[] args) {
+
+    }
+        public static void openSomething(){
+
+            DesiredCapabilities cap = new DesiredCapabilities();
+            cap.setCapability("de", cap);
+        }
+    
+}
